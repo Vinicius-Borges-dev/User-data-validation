74db70e29ff6baa1bec7be1429dba93474c6b781
Vinícius <dev.vinicius.borges@outlook.com>
2024-04-23 13:55:59 -0300

    Update index.js
    Documentando o que as principais linhas de comando executam.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
+// Utilizando o cors para criar uma conexão segura entre o navegador e a API.
 app.use(cors());
+
+// Usando expresss.json para a API entender os resultados no formato JSON.
 app.use(express.json());
 
+// Usando as rotas do diretório Router.
 app.use(authRouter);
 
+// Setando uma porta como padrão caso não esteja setada no arquivo .env 
 const PORT = process.env.PORT || 9090;
 
+// Fazebdo a conexão com a porta setada
 app.listen(PORT, () => {
   console.log(`Servidor rodando no endereço: http://localhost:${PORT}`);
 });
